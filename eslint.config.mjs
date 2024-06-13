558d5e20b5e5e1753abbe0c67b5fd090617967e0
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-06-14 02:12:52 +0900

    🩹 fix icon (#7)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import config from '@woohm402/eslint-config-base';
 
 export default [
   {
-    ignores: ['esbuild.js', 'out', 'dist'],
+    ignores: ['esbuild.js', 'out', 'dist', 'node_modules', '.vscode-test'],
   },
   ...config,
 ];
